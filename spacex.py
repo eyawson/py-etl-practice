f70516a95370c390758501b2d710489d093fcf7b
eyawson <ebow.yawson@gmail.com>
2021-01-25 18:52:03 -0500

    Added mission id number

--- spacex.py
+++ spacex.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import itertools
 
 url = "https://api.spacexdata.com/v3/launches"
 
@@ -15,23 +16,25 @@ def getLaunchData(url):
 
 def tranformData(launch_data):
     # print(launch_data[0]["mission_name"])
+    mission_flight_number = []
     mission_name = []
     mission_image = []
     mission_id = []
 
     for launch in launch_data:
+        mission_flight_number.append(launch["flight_number"])
         mission_name.append(launch["mission_name"])
         mission_image.append(launch["links"]["mission_patch"])
-        mission_id.append(launch["flight_number"])
+        mission_id.append(launch["mission_id"])
 
     """ print(len(mission_image))
     print(len(mission_name))
     print(len(mission_id)) """
 
     with open('rocket_data.csv', 'w', newline='') as data:
-        for a, b, c in zip(mission_id, mission_name, mission_image):
+        for a, b, c, d in itertools.zip_longest(mission_flight_number, mission_name, mission_image, mission_id, fillvalue="empty"):
             add_data = csv.writer(data)
-            add_data.writerow([a, b, c])
+            add_data.writerow([a, b, c, d])
 
 
 # This where we load
